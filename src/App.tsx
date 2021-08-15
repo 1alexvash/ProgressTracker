d6327063f333d227be7df26ac9f7450bd1808109
Alexander <1alexvash@gmail.com>
2021-08-15 16:07:57 +0300

    Added very basic todo functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 
 interface Props {}
 
+type TaskArray = string[];
+
 const App = (props: Props) => {
+  const [tasks, setTasks] = useState<TaskArray>([]);
+
+  const addTask = () => {
+    const taskName = window.prompt("Name of the task")?.trim();
+    if (taskName) {
+      setTasks([...tasks, taskName]);
+    }
+  };
+
   return (
     <div>
+      <ul>
+        {tasks.map((task) => (
+          <li>{task}</li>
+        ))}
+      </ul>
       <h1>Hello from the app!</h1>
+      <button onClick={() => addTask()}>Add Task</button>
     </div>
   );
 };
