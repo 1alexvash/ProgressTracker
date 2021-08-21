58bef9f8af4947ee3c80695fecd53be1a483c995
Alexander <1alexvash@gmail.com>
2021-08-21 13:12:13 +0300

    Added actions and state to the store

---
+++ src/store/actions.tsx
@@ -0,0 +1,5 @@
+import { action, thunk } from "easy-peasy";
+
+const actions = {};
+
+export default actions;
--- src/store/store.tsx
+++ src/store/store.tsx
@@ -1,5 +1,10 @@
 import { createStore } from "easy-peasy";
+import state from "./state";
+import actions from "./actions";
 
-const store = createStore({});
+const store = createStore({
+  ...state,
+  ...actions,
+});
 
 export default store;
