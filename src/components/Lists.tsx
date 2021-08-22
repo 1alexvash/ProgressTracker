1fd497070817dcadbf83f2bd2df96525222d1423
Alexander <1alexvash@gmail.com>
2021-08-22 08:04:27 +0300

    Started immplementing TS in the easy-peasy

--- src/components/Lists.tsx
+++ src/components/Lists.tsx
@@ -1,57 +1,50 @@
-import React, { useState } from "react";
+import React from "react";
+import { useStoreState } from "easy-peasy";
+
 import Task from "./Task";
 
 interface Props {}
 
 const Lists = () => {
-  const [lists, setLists] = useState<ListInteface[]>([
-    {
-      name: "Personal Tasks",
-      tasks: [
-        { name: "Task number, 1" },
-        { name: "Task number, 2" },
-        { name: "Task number, 3" },
-      ],
-    },
-  ]);
+  const { lists } = useStoreState((state) => state);
 
-  const addTask = (listIndex: number) => {
-    const taskName = window.prompt("Name of the task")?.trim();
-    if (taskName) {
-      const newTask = {
-        name: taskName,
-      };
-      const newLists = lists;
-      newLists[listIndex].tasks.push(newTask);
-      setLists([...newLists]);
-    }
-  };
+  // const addTask = (listIndex: number) => {
+  //   const taskName = window.prompt("Name of the task")?.trim();
+  //   if (taskName) {
+  //     const newTask = {
+  //       name: taskName,
+  //     };
+  //     const newLists = lists;
+  //     newLists[listIndex].tasks.push(newTask);
+  //     setLists([...newLists]);
+  //   }
+  // };
 
-  const addList = () => {
-    const listName = window.prompt("Name of the task")?.trim();
-    if (listName) {
-      const newList = {
-        name: listName,
-        tasks: [],
-      };
-      setLists([...lists, newList]);
-    }
-  };
+  // const addList = () => {
+  //   const listName = window.prompt("Name of the task")?.trim();
+  //   if (listName) {
+  //     const newList = {
+  //       name: listName,
+  //       tasks: [],
+  //     };
+  //     setLists([...lists, newList]);
+  //   }
+  // };
 
   return (
     <div className="lists">
       {lists.map((list, listIndex) => (
-        <div className="list">
+        <div className="list" key={listIndex}>
           <h2>{list.name}</h2>
           <div className="tasks">
-            {list.tasks.map((task: TaskInteface) => (
-              <Task task={task} />
+            {list.tasks.map((task: TaskInteface, taskIndex: number) => (
+              <Task task={task} key={taskIndex} />
             ))}
-            <button onClick={() => addTask(listIndex)}>Add Task</button>
+            {/* <button onClick={() => addTask(listIndex)}>Add Task</button> */}
           </div>
         </div>
       ))}
-      <button onClick={() => addList()}>Add List</button>
+      {/* <button onClick={() => addList()}>Add List</button> */}
     </div>
   );
 };
--- src/store/state.tsx
+++ src/store/state.tsx
@@ -1,3 +1,22 @@
-const state = {};
+const state = {
+  lists: [
+    {
+      name: "Personal Tasks",
+      tasks: [
+        { name: "Task number, 1" },
+        { name: "Task number, 2" },
+        { name: "Task number, 3" },
+      ],
+    },
+    {
+      name: "Job Tasks",
+      tasks: [
+        { name: "Job number, 1" },
+        { name: "Job number, 2" },
+        { name: "Job number, 3" },
+      ],
+    },
+  ],
+};
 
 export default state;
