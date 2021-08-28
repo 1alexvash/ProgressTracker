79d205e8a963877de505a1bfea7d390d189c056b
Alexander <1alexvash@gmail.com>
2021-08-28 16:30:30 +0300

    Added Alias

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 import { StoreProvider, useStoreState, useStoreActions } from "easy-peasy";
 import store from "./store/store";
-import { StoreModel } from "./store/StoreModel";
+import { StoreModel } from "@/store/StoreModel";
 
 function TodoList() {
   const todos = useStoreState<StoreModel>((state) => state.todos);
